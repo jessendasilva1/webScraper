2042c64471806cd0ae74e75b302977ccd608e73b
Jessen <jessendasilva1@gmail.com>
2019-04-01 19:47:04 -0400

    basic webscraping implemented

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,31 @@
-let db = require("../models");
-const axios = require('axios');
+//let db = require("../models");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-module.exports = function (app) {
-    app.get('/', function(req, res) {
-        res.send("hello");
-    })
-        /*
-        axios.get("/")
-            .then((res) => {
-                res.send("hello from root");
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-            */
-}
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    axios
+      .get(
+        "https://www.newegg.ca/Gaming-Desktops/SubCategory/ID-3742?Tid=897450"
+      )
+      .then(res => {
+        //let results = [];
+        let $ = cheerio.load(res.data);
+        $("div.item-container").each(function(index, element) {
+          let link = $(element)
+            .children()
+            .attr("href");
+          console.log(link);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    res.render("index");
+  });
+
+  //The 404 Route (ALWAYS Keep this as the last route)
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
--- server.js
+++ server.js
@@ -1,42 +1,44 @@
 require("dotenv").config();
-const express = require('express');
-const mongoose = require('mongoose');
-const exphbs = require('express-handlebars');
-const cheerio = require("cheerio");
-const db = require('./models');
+var express = require("express");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+//var cheerio = require("cheerio");
+//var db = require("./models");
 
-const app = express();
+var app = express();
 var PORT = process.env.PORT || 4000;
 
 // Parse request body as JSON
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 // Handlebars
 app.engine(
-    "handlebars",
-    exphbs({
-        defaultLayout: "main",
-        partialsDir: __dirname + "/views/partials/"
-    })
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    partialsDir: __dirname + "/views/partials/"
+  })
 );
 app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webScraperDB";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webScraperDB";
 
 mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 
 // Start the server
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
 
 module.exports = app;
